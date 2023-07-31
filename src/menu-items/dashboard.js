f802784c9ddf5c82c1d931a5476b83b62a6cba80
obaid ur rehman <120463560+ObaidHere@users.noreply.github.com>
2023-07-31 16:15:08 +0500

    dashboard navigation

--- src/menu-items/dashboard.js
+++ src/menu-items/dashboard.js
@@ -22,7 +22,7 @@ const dashboard = {
       id: 'default',
       title: 'Dashboard',
       type: 'item',
-      url: '/dashboard',
+      url: '/',
       icon: GridViewIcon,
       breadcrumbs: false
     },
