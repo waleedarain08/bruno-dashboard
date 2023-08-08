045094bcb50da52f142ed756e5b9ee24b9f2d12c
obaid ur rehman <94833027+obaidrehmann@users.noreply.github.com>
2023-08-09 03:45:41 +0500

    running alpha build

--- src/store/auth/authReducer.js
+++ src/store/auth/authReducer.js
@@ -2,35 +2,33 @@
 import * as actionTypes from './authType';
 
 export const initialState = {
-    isLoading: false,
-    data: null
-
+  isLoading: false,
+  data: null
 };
 
 // ==============================|| AuthReducer REDUCER ||============================== //
 
 const AuthReducer = (state = initialState, action) => {
-    let id;
-    switch (action.type) {
-        case actionTypes.Loading:
-            return {
-                ...state,
-                isLoading: true
-            };
-        case actionTypes.SuccessLogin:
-            return {
-                ...state,
-                isLoading: false,
-                data: action.payload
-            };
-        case actionTypes.FailedLogin:
-            return {
-                ...state,
-                isLoading: false,
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case actionTypes.Loading:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case actionTypes.SuccessLogin:
+      return {
+        ...state,
+        isLoading: false,
+        data: action.payload
+      };
+    case actionTypes.FailedLogin:
+      return {
+        ...state,
+        isLoading: false
+      };
+    default:
+      return state;
+  }
 };
 
 export default AuthReducer;
