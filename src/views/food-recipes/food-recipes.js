ac04a52c8898add0a55fd2475f12e9742e4a6893
Waleed Arain <77847857+waleedarain08@users.noreply.github.com>
2023-09-14 02:25:44 +0500

    rows.recipe.map

--- src/views/food-recipes/food-recipes.js
+++ src/views/food-recipes/food-recipes.js
@@ -471,7 +471,7 @@ const FoodRecipes = () => {
           </Box>
           <Box sx={{ flexGrow: 1 }}>
             <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-              {rows?.map((i, index) => {
+              {rows?.recipe?.map((i, index) => {
                 return (
                   <Grid item xs={2} sm={4} md={4} key={index}>
                     <RecipeReviewCard data={i} key={index} setOpen={setOpen} EditValues={EditValues} />
