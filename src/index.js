c3f6e5b25493ff54cb542e61c2d80f19a554d5e6
Waleed Arain <77847857+waleedarain08@users.noreply.github.com>
2024-01-05 18:45:07 +0500

    push to live link .

--- src/index.js
+++ src/index.js
@@ -32,3 +32,5 @@ root.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+//Make it Live
