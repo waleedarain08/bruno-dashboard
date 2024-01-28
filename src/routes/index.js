cfe0ea8939445cfef94fec6fc897db1387d458e0
Waleed Arain <77847857+waleedarain08@users.noreply.github.com>
2024-01-29 02:51:56 +0500

    fixed no-used cars issue

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import { useRoutes } from 'react-router-dom';
-import { useContext } from 'react';
-import { UserContext } from '../context/userContext';
+//import { useContext } from 'react';
+//import { UserContext } from '../context/userContext';
 // routes
 import MainRoutes from './MainRoutes';
 import AuthenticationRoutes from './AuthenticationRoutes';
