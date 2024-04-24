31560afaa218ab3f5f618b6323032b35305caa55
Waleed Arain <77847857+waleedarain08@users.noreply.github.com>
2024-04-25 03:53:44 +0500

    added order date and time

--- src/views/order-list/OrderList.js
+++ src/views/order-list/OrderList.js
@@ -429,7 +429,7 @@ export default function OrderList() {
               <TableRow style={{ backgroundColor: '#D78809' }}>
                 <TableCell />
                 <TableCell style={{ color: '#fff' }}>Order No.</TableCell>
-                {/* <TableCell style={{ color: '#fff' }}>Order Date / Time</TableCell> */}
+                <TableCell style={{ color: '#fff' }}>Order Date / Time</TableCell>
                 <TableCell style={{ color: '#fff' }}>User Name</TableCell>
                 <TableCell style={{ color: '#fff' }} align="right">
                   Order Total (AED)
